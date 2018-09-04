c881bbe72145cfdd302bddc0066860067729b7fb
abyy14 <34206907+abyy14@users.noreply.github.com>
2018-09-04 15:46:49 +0530

    Add files via upload

---
+++ MaximumElement.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prime;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author MSAJCE-CSE31
+ */
+public class MaximumElement {
+    public static void main(String[] args){
+        try{
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the array size: ");
+        int size=(int)s.nextDouble();
+        double[] arr=new double[size];
+       System.out.println("Enter the array elements: ");
+       for(int i=0;i<arr.length;i++){
+           arr[i]=s.nextDouble();
+           
+       }
+       for(int i=0;i<arr.length-1;i++){
+           for(int j=1;j<arr.length;j++){
+               if(arr[j]>arr[i]){
+                   double temp=arr[i];
+                   arr[i]=arr[j];
+                   arr[j]=temp;
+               }
+           }
+          
+       }
+       System.out.println("Maximum element");
+        System.out.println(arr[0]);
+    }
+    
+    catch(InputMismatchException e){
+    System.out.println("Enter correct input");
+}
+}
+}
