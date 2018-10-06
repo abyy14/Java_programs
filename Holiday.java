66df1546fbad0ef0ee484b87ab89c65ba55d08df
abyy14 <34206907+abyy14@users.noreply.github.com>
2018-10-06 12:07:50 +0530

    Create Holiday.java

---
+++ Holiday.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculation;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/**
+ *
+ * @author Sarala
+ */
+public class Holiday {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner s= new Scanner(System.in);
+        String str=s.nextLine();
+        if(str=="Saturday"||str=="Sunday"){
+           System.out.print("yes");
+           // System.out.println(arr[i]);
+                    
+        }else{
+            System.out.print("no");
+        }
+        
+        
+    }
+    
+}
